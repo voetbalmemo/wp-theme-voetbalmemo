0ed8d006057ae0f756d0e2c4f1f0e97cd47f3a0f
s1136647 <s1136647@student.hsleiden.nl>
2023-11-12 11:48:51 +0100

    added footer

--- footer.php
+++ footer.php
@@ -1,5 +1,29 @@
-<?php wp_footer();?>
+        <div>
+            <footer>
+                <div>
+                    <nav>
+                        <?php wp_nav_menu(array(
+                            'container' => 'div',                           // enter '' to remove nav container (just make sure .footer-links in _base.scss isn't wrapping)
+                            'container_class' => 'footer-links cf',         // class of container (should you choose to use it)
+                            'menu' => __( 'Footer Links', 'bonestheme' ),   // nav name
+                            'menu_class' => 'nav footer-nav cf',            // adding custom nav class
+                            'theme_location' => 'footer-links',             // where it's located in the theme
+                            'before' => '',                                 // before the menu
+                            'after' => '',                                  // after the menu
+                            'link_before' => '',                            // before each link
+                            'link_after' => '',                             // after each link
+                            'depth' => 0,                                   // limit the depth of the nav
+                            'fallback_cb' => 'bones_footer_links_fallback'  // fallback function
+                        )); ?>
+                    </nav>
+                    <p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'VoetbalMemo' ); ?>.</p>
+                </div>
+            </footer>
+        </div>
 
+        <?php // all js scripts are loaded in library/bones.php ?>
+        <?php wp_footer(); ?>
 
-</body>
+    </body>
 </html>
+<!--End of the site-->
